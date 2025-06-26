3d1d7f04344f57a064b9062ddbbb43987c2d7aa6
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-06-26 14:16:39 -0400

    Create exampleClass.cpp

---
+++ Practice/OOP/exampleClass.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Animal {
+// Public Access Specifier
+public:
+    string species;
+    int age;
+    int name;
+
+    // Member functions
+    void eat() {
+        // eat something
+    }
+
+    void sleep() {
+        // sleep for a few hours
+    }
+
+    void makeSound() {
+        // make sound
+    }
+};
