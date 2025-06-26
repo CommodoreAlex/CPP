8dc772640a2e7bc02a6260c6c202428e997ea209
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-06-26 14:23:18 -0400

    Create exampleObject.cpp

---
+++ Practice/OOP/Basics/exampleObject.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    string species;
+    int age;
+    int name;
+
+    // Member functions
+    void eat() {
+        // eat something
+    }
+
+    void sleep() {
+        // sleep for a few hours
+    }
+
+    void makeSound() {
+        // make sound
+    }
+};
+
+int main() {
+    // Introducing Objects
+    Animal VoidShadowDarkFangReaper;
+}
