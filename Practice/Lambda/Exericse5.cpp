395cf1f8b2a622624da7937c96826c7d40fe4d0e
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-12-29 11:05:04 -0800

    Update Exericse5.cpp

--- Practice/Lambda/Exericse5.cpp
+++ Practice/Lambda/Exericse5.cpp
@@ -1,4 +1,10 @@
 #include <type_traits>
+#include <cassert>
+
+  int divide(int a, int ) {
+    assert(b != 0); // ensure no divison by zero
+    return a / b;
+  }
 
 int main() {
 
@@ -12,4 +18,8 @@ int main() {
 
   // Like an assert, but checks things at compile-time instead of run-time
   static_assert(std::is_same<double, decltype(baz(10))>::value, "has to be the same!");
+
+  // An assert, run-time failure
+  divide(10, 2);
+  divide(10, 0);  // Fail here
 }
