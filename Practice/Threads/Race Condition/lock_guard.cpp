a252703f5f550efc431bc81539179a2a97d48eda
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2026-01-06 15:14:56 -0500

    Create lock_guard.cpp

---
+++ Practice/Threads/Race Condition/lock_guard.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+// Automatically locking the mutex when created and unlocks it when it goes out of scope (safer than convetional locking mechanism)
+
+int counter = 0;
+std::mutex m;
+
+void worker() {
+  for (int i = 0; i < 100000; i++) {
+    m.lock();    // lock before touching shared data
+    counter++;   // critical section
+    m.unlock();  // unlock after
+  }
+}
+
+int main() {
+  std::thread t1(worker);
+  std::thread t2(worker);
+
+  t1.join();
+  t2.join();
+
+  std::cout << "Final Counter: " << counter << std::endl;
+  return 0;
+}
