08aa28aedac6e87dd275aa16c6e336961412a933
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2026-01-10 19:13:36 -0800

    Create passToThread.cpp

---
+++ Practice/Lambda/C++17/Threads/passToThread.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <numeric> // for std::iota
+
+int main() {
+  const auto printThreadID = [](const char* str) {
+    std::cout << str << ": "
+              << std::this_thread::get_id() << " thread id\n";
+  };
+
+  std::vector<int> numbers(100);
+
+  std::thread iotaThread([&numbers, &printThreadID](int startArg) {
+    std::iota(numbers.begin(), numbers.end(), startArg);
+    printThreadID("iota in");
+  }, 10);
+
+  iotaThread.join();
+  printThreadID("printing numbers in");
+
+  for (const auto& num : numbers) {
+    std::cout << num << ", ";
+  }
+}
