4d59f40d8a1c04908f8e37ebe22659b45575a238
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2026-01-11 07:50:57 -0800

    Create structuredBinding.cpp

---
+++ Practice/Lambda/C++20/structuredBinding.cpp
@@ -0,0 +1,12 @@
+#include <tuple>
+#include <string>
+
+auto GetParams() {
+  return std::tuple { std::string{"Hello World"}, 42};
+}
+
+int main() {
+  auto [x, y] = GetParams();
+  const auto ParamLength = [&x, &y]() { return x.length() + y; }();
+  return ParamLength;
+}
