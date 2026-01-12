232a99b1d69f75f1d9837573344a202f7891b271
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2026-01-12 14:34:02 -0800

    Update CTAD.cpp

--- Practice/C++17/Map/CTAD.cpp
+++ Practice/C++17/Map/CTAD.cpp
@@ -8,8 +8,12 @@ int main() {
   // Class Template Argument Deducation (CTAD)
   std::map mapCopy{mapUsersAge};
 
+  // If the key does not exist, it inserts it and returns {iterator,true}
+  // If the key already exists, it assigns the new value and returns {iterator,false}
+  // Structured binding unpacks the pair returned by insert_or_assign
   if (auto [iter, wasAdded] = mapCopy.insert_or_assign("John", 26); !wasAdded) std::cout << iter->first << " reassigned...\n";
 
+  // Structured Binding [key, value]
   for (const auto& [key, value] : mapCopy) {
     std::cout << key << ", " << value << '\n';
   }
